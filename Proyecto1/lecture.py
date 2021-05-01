7e5e6e8f9f977cdfb0330591402b8c14564b5c1a
caarroyavepadilla <62408347+caarroyavepadilla@users.noreply.github.com>
2021-04-30 19:08:24 -0600

    todo el proyecto 2

---
+++ Proyecto1/lecture.py
@@ -0,0 +1,135 @@
+def word_reader(file, current):
+    temp = ""
+    while current < len(file):
+        if(file[current] == " " or file[current] == "\n") and (len(temp) > 0):
+            break
+        elif file[current] == " " or file[current] == "\n":
+            current += 1
+        else:
+            temp += file[current]
+            current += 1
+
+    return temp, current
+
+
+def readerino(file):
+    a = 1
+    current = 0
+    chars = []
+    tokens = []
+    keywords = []
+    prods = []
+    temp = ""
+    while True:
+        temp, current = word_reader(file, current)
+        if temp == "COMPILER":
+            title, current = compiler(file, current)
+        if temp == "CHARACTERS":
+            chars, current = charas(file, current)
+        if temp == "KEYWORDS":
+            keywords, current = keys(file, current)
+        if temp == "TOKENS":
+            tokens, current = toks(file, current)
+        if temp == "END":
+            endof = endofline(file, current, title)
+            if endof:
+                break
+            else:
+                print("no hay final de archivo")
+                break
+
+    return title, chars, tokens, keywords, prods
+
+
+def compiler(file, current):
+    current += 1
+    title, current = word_reader(file, current)
+    return title, current
+
+
+def charas(file, current):
+
+    current += 1
+    charass = {}
+    line = ""
+
+    while True:
+        temp, current = word_reader(file, current)
+        if temp == "KEYWORDS":
+            current -= 8
+            break
+        line += temp
+        if line[-1] == "." and line[-2] != ".":
+            if "=" in line:
+                alles = line.split("=")
+                ids = alles[0]
+                vals = alles[1]
+                charass[ids] = vals
+                line = ""
+            else:
+                print("'=' no encontrado")
+    return charass, current
+
+def keys(file, current):
+    current += 1
+    temp = ""
+    keyes = {}
+    ids = ""
+    vals = ""
+    line = ""
+    while True:
+        temp, current = word_reader(file, current)
+        if temp == "TOKENS":
+            current -= 6
+            break
+        line += temp
+        if line[-1] == ".":
+            if "=" in line:
+                alles = line.split("=")
+                ids = alles[0]
+                vals = alles[1]
+                keyes[ids] = vals
+                line = ""
+            else:
+                print("'=' no encontrado")
+
+    return keyes, current
+
+
+def toks(file, current):
+    current += 1
+    temp = ""
+    tokis = {}
+    ids = ""
+    vals = ""
+    line = ""
+    while True:
+        temp, current = word_reader(file, current)
+        if temp == "PRODUCTIONS":
+            current -= 11
+            break
+        if temp == "END":
+            current -= 3
+            break
+        line += temp
+        if line[-1] == ".":
+            if "=" in line:
+                alles = line.split("=")
+                print("este es alles", alles)
+                ids = alles[0]
+                vals = alles[1]
+                print(alles[1])
+                tokis[ids] = vals
+                line = ""
+            else:
+                print("'=' no encontrado")
+
+    return tokis, current
+
+
+def endofline(file,current, title):
+    current += 1
+    enderino, current = word_reader(file,current)
+    if enderino == title:
+        return True
+    return False
